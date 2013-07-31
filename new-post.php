aa39593da307d2945ddd89b17ab3ca55e80db697
DK <dukai86@gmail.com>
2013-08-01 01:20:36 +0800

    添加发布页面

---
+++ new-post.php
@@ -0,0 +1,33 @@
+<?
+if(empty($_SERVER['HTTP_AJAXTYPE'])){
+?>
+<?include "header.php"?>
+<div id="mainbody">
+<?}?>
+
+	<div class="form_box">
+		<form>
+		<div class="fields_box">
+			<div class="cline">
+				<label>标题</label>
+				<input type="text" name="subject" />
+			</div>
+			
+			<div class="cline">
+				<label>内容</label>
+				<textarea name="content"></textarea>
+			</div>
+		</div>
+		<div class="cline">
+			<button class="btn">确定</button>
+			<button class="btn_gray">取消</button>
+		</div>
+		</form>
+	</div>
+
+<?
+if(empty($_SERVER['HTTP_AJAXTYPE'])){
+?>
+</div>
+<?include "footer.php"?>
+<?}?>
