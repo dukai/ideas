48bfa46e49c120c2e0b5d1ec99481de999770c8a
DK <dukai86@gmail.com>
2013-08-02 01:47:09 +0800

    添加边栏

---
+++ aside.php
@@ -0,0 +1,11 @@
+<aside>
+	<div>
+		<a href="/new-post.php" class="btn pjax" data-pjax="#maincontent">New Post</a>
+	</div>
+	
+	<section class="hot">
+		<ul>
+			<li></li>
+		</ul>
+	</section>
+</aside>
